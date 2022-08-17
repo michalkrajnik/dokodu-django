5685022499258c9dbe8dbfdc2f42ee3e1674b2cd
michalkrajnik <michal.krajnik.pl@gmail.com>
2022-08-17 11:51:30 +0200

    Create main app

--- posts/models.py
+++ posts/models.py
@@ -5,3 +5,10 @@ from django.db import models
 
 class Post(models.Model):
     author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'/{self.id} {self.title}'
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from posts.models import Post
+
+
+def first_post(request):
+    post = Post.select.first()
+     

78dc18504c0f14deaa60148bf351f54c7a55c2a2
michalkrajnik <michal.krajnik.pl@gmail.com>
2022-08-17 12:10:16 +0200

    Add field published to Post

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
-# Create your models here.
 
 class Post(models.Model):
     author = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
+    published = models.BooleanField(default=False)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
